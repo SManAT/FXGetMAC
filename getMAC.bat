845ade58e889b04c43d90d1457cbe585d9961773
SManAT <s.hagmann@bgbaden-frauen.ac.at>
2019-08-20 19:01:39 +0200

    wrapper

--- getMAC.bat
+++ getMAC.bat
@@ -1,28 +1,10 @@
+
 @echo off
 @pushd %~dp0
-setlocal ENABLEEXTENSIONS
-set KEY_NAME="HKLM\SOFTWARE\JavaSoft\Java Runtime Environment"
-set VALUE_NAME=CurrentVersion
-::
-:: get the current version
-::
-FOR /F "usebackq skip=2 tokens=3" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
-    set ValueValue=%%A
-)
 
-set JAVA_CURRENT="HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\%ValueValue%"
-set JAVA_HOME=JavaHome
-::
-:: get the javahome
-::
-FOR /F "usebackq skip=2 tokens=3*" %%A IN (`REG QUERY %JAVA_CURRENT% /v %JAVA_HOME% 2^>nul`) DO (
-    set JAVA_PATH=%%A %%B
-)
+set JAVA_PATH="C:\Program Files\Java\jdk1.8.0_162"
 
 powershell.exe Start-Process -FilePath '"%JAVA_PATH%\bin\javaw.exe"' -Argument '-jar FXGetMAC.jar' -verb RunAs
 
 ::unmount
 @popd
-
-:end
-
